f068f8a2c1e4ee118f38eadf039a0ca93a797c64
Ki Ki  Bush <donyabush@gmail.com>
2018-07-31 00:41:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function capture(e) {
  
 for (var aDiv of divs) {
   // set the third argument to `true`!
-  adiv.addEventListener('click', bubble);
+  aDiv.addEventListener('click', capture, true)
 }
 
 
